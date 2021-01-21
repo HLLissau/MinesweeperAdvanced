feab5156c082eaa840004e9434b5c4c167204483
Harald <Harald@192.168.0.41>
2021-01-21 11:47:25 +0100

    Button commented

--- src/MinesweeperButton.java
+++ src/MinesweeperButton.java
@@ -9,7 +9,10 @@ public class MinesweeperButton extends Button{
 	private Point position;
 	private ArrayList<MinesweeperButton> neightbours;
 	public Boolean flag;
-
+	
+	/*
+	 * contructor. set button size, Point position and create Arraylist<>
+	 */
 	public MinesweeperButton(int x, int y) {
 		this.position = new Point(x,y);
 		neightbours = new ArrayList<MinesweeperButton>() ;
@@ -20,15 +23,23 @@ public class MinesweeperButton extends Button{
 		this.setMaxHeight(25.0);
 	}
 	
+	/*
+	 * Output: Point position
+	 */
 	public Point getPos() {
 		return this.position;
 	}
 	
+	/*
+	 * Output ArrayList with neighbours
+	 */
 	public ArrayList<MinesweeperButton> getneighbours() {
 		return this.neightbours;
 	}
     
-	
+	/*
+	 * Changes flag boolean
+	 */
 	public void changeFlag() {
 		if (flag) {
 			this.flag = false ;
@@ -39,12 +50,16 @@ public class MinesweeperButton extends Button{
 		
 	}
 	
-	public void setNeighbours(int x, int y, ObservableList<Node> list) {
+	/*
+	 * Sets ArrayList<MinesweeperButton> with neighbouring buttons. Used for autoclear.
+	 * Input:  m,n are game.model size, Observable list of all MinesweeperButtons 
+	 */
+	public void setNeighbours(int m, int n, ObservableList<Node> list) {
 		for (int k = -1; k <= 1; k++) {
 			for (int l =-1; l <= 1; l++) {
 				if (k!=0 || l!=0) {
-					if (this.position.y+k >= 0 && this.position.y+k < y && this.position.x+l >=0 && this.position.x+l < x) {
-						MinesweeperButton temp = (MinesweeperButton) list.get(((position.x+l)+((position.y+k)*x)));
+					if (this.position.y+k >= 0 && this.position.y+k < n && this.position.x+l >=0 && this.position.x+l < m) {
+						MinesweeperButton temp = (MinesweeperButton) list.get(((position.x+l)+((position.y+k)*m)));
 						this.neightbours.add(temp);
 						
 					}
