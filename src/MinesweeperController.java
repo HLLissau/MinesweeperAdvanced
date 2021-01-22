1aefcd18096db33aa5f73a694dc28428314288b1
Erik Falk <s204438@student.dtu.dk>
2021-01-22 11:57:14 +0100

    Removed unnecessary test print.

--- src/MinesweeperController.java
+++ src/MinesweeperController.java
@@ -240,7 +240,6 @@ public class MinesweeperController  {
 			 Scanner scanner = new Scanner(file);
 			 for (int i=0; i<40;i++) {
 				 loadedhighscore.add(scanner.next());
-				 System.out.println(loadedhighscore.get(i));
 			 }
 			 scanner.close();
 	     } else {
